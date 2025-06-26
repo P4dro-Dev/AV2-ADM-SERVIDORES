5c0bfc5b57f600e62aac84ddf2670d50be6fd59a
Pedro Henrique <Ph9084533@gmail.com>
2025-06-26 14:26:54 -0300

    Add files via upload
    Adicionando ao repositório a avaliação completa, e respondida!

---
+++ AT2-Admistração-de-Servidores/Q1.js
@@ -0,0 +1,5 @@
+// Problema: Crie um programa que imprima no console os números de 1 a 5.
+// O que usar: Um loop for tradicional é ideal para iterar um número fixo de vezes ou para controlar um contador.
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
