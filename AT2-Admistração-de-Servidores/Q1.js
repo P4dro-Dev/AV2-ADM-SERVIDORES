69e236b881406af456a69fb9aafca499ae3a394c
Pedro Henrique <Ph9084533@gmail.com>
2025-06-26 17:32:27 +0000

    Organizando os arquivos nas pastas, e colocando identificação básica no projeto

--- AT2-Admistração-de-Servidores/Q1.js
+++ AT2-Admistração-de-Servidores/Q1.js
@@ -1,3 +1,9 @@
+// Discente: Pedro Henrique Sousa da Silva
+// Curso: TI
+// Docente: Dr. Alexandre
+
+// Tarefa avaliativa da disciplina de Administração de Servidores. 
+
 // Problema: Crie um programa que imprima no console os números de 1 a 5.
 // O que usar: Um loop for tradicional é ideal para iterar um número fixo de vezes ou para controlar um contador.
 for (let i = 1; i <= 5; i++) {
